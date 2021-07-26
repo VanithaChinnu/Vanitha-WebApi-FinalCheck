0eb08e388a46dc34042c3858ee8888a4da0566a0
Vanitha <vanitha4963@gmail.com>
2021-07-26 13:46:28 +0530

    Changed

--- MovieLibrary/MovieDataContext.cs
+++ MovieLibrary/MovieDataContext.cs
@@ -18,7 +18,6 @@ namespace MovieLibrary
 
         }
         public virtual DbSet<Movie> Movies { get; set; }
-        public virtual DbSet<User> Users { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
